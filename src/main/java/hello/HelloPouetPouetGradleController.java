b937dff8b14a48184fe492120aed4c3db06bafa0
Segolene Bauque <sego.bauque@gmail.com>
2019-08-20 16:18:19 +0200

    optimisation des imports

--- src/main/java/hello/HelloPouetPouetGradleController.java
+++ src/main/java/hello/HelloPouetPouetGradleController.java
@@ -1,6 +1,5 @@
 package hello;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/hello/AppTest.java
+++ src/test/java/hello/AppTest.java
@@ -7,9 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
